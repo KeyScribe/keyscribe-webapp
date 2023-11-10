e6e1d90a7e3b56de2c7f98282f22de0964b1f54f
Adam Horton <adam.horton@ufl.edu>
2023-11-10 13:32:38 -0500

    wrapped onClick functions

--- backend/src/routes/led-router.ts
+++ backend/src/routes/led-router.ts
@@ -1,9 +1,7 @@
 import { Router } from 'express';
-import { ledOn, keyboard } from '../handlers/led-handler';
+import { ledOn } from '../handlers/led-handler';
 
 const router = Router();
-
-router.get('/keyboard', keyboard);
 
 router.post('/led', ledOn);
 
