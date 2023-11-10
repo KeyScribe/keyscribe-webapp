e566bde01f5104e638582fbdbb4e897b20a70c33
Lorenz Carvajal <lorenzcarvajal@ufl.edu>
2023-11-10 14:39:22 -0500

    Organized Login page into component structure

---
+++ frontend/src/components/Login/Login.stories.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import Login from './Login';
+
+export default {
+  title: "Login",
+};
+
+export const Default = () => <Login />;
+
+Default.story = {
+  name: 'default',
+};
