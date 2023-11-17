d19c712309e87d37ea589255fdd980472897f972
Viraj Chandrasekaran <58404548+vchandrasekaran1@users.noreply.github.com>
2023-11-17 17:17:11 -0500

    Update Keyboard and Timer

--- frontend/src/components/Keyboard/Keyboard.js
+++ frontend/src/components/Keyboard/Keyboard.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
 import { KeyboardWrapper } from './Keyboard.styled';
 import Button from '@mui/material/Button';
+import 'react-piano/dist/styles.css';
 
 const sendRequest = (pin, state) => {
     fetch('/keyboard/led', {
@@ -25,9 +27,39 @@ const sendRequest = (pin, state) => {
     });
 }
 
-const Keyboard = () => {
+const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const formattedSeconds = remainingSeconds < 10 ? `0${remainingSeconds}` : remainingSeconds;
+    return `${formattedMinutes}:${formattedSeconds}`;
+};
 
+const Keyboard = () => {
+ const firstNote = MidiNumbers.fromNote('c3');
+ const lastNote = MidiNumbers.fromNote('f3');
+ const keyboardShortcuts = KeyboardShortcuts.create({
+      firstNote: firstNote,
+      lastNote: lastNote,
+      keyboardConfig: KeyboardShortcuts.HOME_ROW,
+ });
  const [isRecording, setRecording] = useState(false);
+ const [timer, setTimer] = useState(0);
+
+ useEffect(() => {
+    let interval;
+
+    if (isRecording) {
+        interval = setInterval(() => {
+            setTimer((prevTimer) => prevTimer + 1);
+        }, 1000);
+    } else {
+        clearInterval(interval);
+        setTimer(0);
+    }
+
+    return () => clearInterval(interval);
+}, [isRecording]);
 
  const startRecording = () => {
     setRecording(true);
@@ -41,17 +73,51 @@ const Keyboard = () => {
 
  return (
     <KeyboardWrapper data-testid="Keyboard" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+
+    {isRecording && (
+         <div style={{ position: 'absolute', top: -175, left: -320, width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+         <div style={{ width: '25px', height: '25px', backgroundColor: 'red', borderRadius: '50%' }}></div>
+     </div>
+    )}
+
+    {isRecording && (
+         <div style={{ position: 'absolute', top: 150, left: 0, width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+         <p>Time Passed - {formatTime(timer)}</p>
+
+     </div>
+    )}  
+
+    {(!isRecording && timer > 0) ? (
+                 <div style={{ position: 'absolute', top: 25, left: 0, width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <p>Recording stopped. Total time: {formatTime(timer)}</p>
+                </div>
+            ) : null}
+
     <Button onClick={startRecording} variant="contained" style={{ textTransform: 'none' }} disabled={isRecording}>Start Recording</Button>
     <div class="piano-container">
         
         <div onMouseDown={() => sendRequest("4", "0")} onMouseUp={() => sendRequest("4", "1")} class="piano-key white" id="greenButton">Green</div>
+        {/* <div onClick={() => sendRequest('green')} class="piano-key white" id="greenButton">Green</div>
         <div class="piano-key black" id="blackButton1">Black</div>
         <div onMouseDown={() => sendRequest("27", "0")} onMouseUp={() => sendRequest("27", "1")} class="piano-key white" id="redButton">Red</div>
         <div class="piano-key black" id="blackButton2">Black</div>
         <div onMouseDown={() => sendRequest("10", "0")} onMouseUp={() => sendRequest("10", "1")} class="piano-key white" id="yellowButton">Yellow</div>
         <div class="piano-key black" id="blackButton3">Black</div>
-        <div onMouseDown={() => sendRequest("9", "0")} onMouseUp={() => sendRequest("9", "1")} class="piano-key white" id="blueButton">Blue</div>
+         <div onClick={() => sendRequest('blue')} class="piano-key white" id="blueButton">Blue</div> */ }
+        <Piano
+          noteRange={{ first: firstNote, last: lastNote }}
+          playNote={(midiNumber) => {
+            // Play a given note - see notes below
+            sendRequest(midiNumber.toString());
+          }}
+          stopNote={(midiNumber) => {
+            // Stop playing a given note - see notes below
+          }}
+          width={500}
+          keyboardShortcuts={keyboardShortcuts}
+        />
     </div>
+
     <Button onClick={stopRecording} variant="contained" style={{ textTransform: 'none' }} disabled={!isRecording}>Stop Recording</Button>
     </KeyboardWrapper>
  );
