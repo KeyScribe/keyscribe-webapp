4948fee81c223abc961dc9884fafe3d24bc4180b
Andrew Liu <aliu2@ufl.edu>
2024-01-23 18:01:25 -0500

    Updated DB pw

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -5,7 +5,7 @@ const pool = new Pool({
   user: 'keyscribe',
   host: 'ec2-3-86-40-33.compute-1.amazonaws.com',
   database: 'ks_db',
-  password: 'Keyscribe',
+  password: 'K3yscr1b3',
   port: 5432, // Default PostgreSQL port
   ssl: false
 });
