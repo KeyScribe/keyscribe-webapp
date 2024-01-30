20d89d8d1206cfc4c726e8188890bc7d0fa14fd5
Adam Horton <adam.horton@ufl.edu>
2024-01-30 11:05:27 -0500

    made password secret

--- backend/src/db/db-setup.ts
+++ backend/src/db/db-setup.ts
@@ -1,10 +1,14 @@
 import { Pool, QueryResult } from 'pg';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 const pool = new Pool({
   user: 'keyscribe',
   host: 'ec2-3-86-40-33.compute-1.amazonaws.com',
   database: 'ks_db',
-  password: 'K3yscr1b3',
+  password: process.env.DB_PASSWORD!,
   port: 5432, // Default PostgreSQL port
   ssl: false,
 });
